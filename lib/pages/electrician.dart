800e647b9671589be79eab0c71a87d7706e0cd9f
ashutoshjak <58728879+ashutoshjak@users.noreply.github.com>
2020-04-20 16:35:15 +0545

    changes in electricain

--- lib/pages/electrician.dart
+++ lib/pages/electrician.dart
@@ -9,6 +9,7 @@ class _ElectricianState extends State<Electrician> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        resizeToAvoidBottomInset: false,
         appBar: new AppBar(
           elevation: 0.2,
           backgroundColor: Colors.black,
@@ -18,31 +19,37 @@ class _ElectricianState extends State<Electrician> {
           padding: const EdgeInsets.all(20.0),
           child: Column(
             children:[
-              new TextField(
-                decoration: new InputDecoration(labelText: 'Enter task',),// labelStyle: TextStyle(color: Colors.black)
-              ),
-              new TextField(
-                decoration: new InputDecoration(labelText: 'Enter location'),
-              ),
-              new TextField(
-                decoration: new InputDecoration(labelText: 'Enter time'),
-              ),
-              new TextField(
-                decoration: new InputDecoration(labelText: 'Enter date'),
-              ),
+              Flexible(
+                child: Form(
+                  child: Column(
+                    children: <Widget>[
+                      new TextField(
+                        decoration: new InputDecoration(labelText: 'Enter task',),// labelStyle: TextStyle(color: Colors.black)
+                      ),
+                      new TextField(
+                        decoration: new InputDecoration(labelText: 'Enter location'),
+                      ),
+                      new TextField(
+                        decoration: new InputDecoration(labelText: 'Enter time'),
+                      ),
+                      new TextField(
+                        decoration: new InputDecoration(labelText: 'Enter date'),
+                      ),
 
-              new RaisedButton(
-                color: Colors.black,
-                onPressed: (){},
-                child: new Text('Cancel',style: TextStyle(color: Colors.white),),
-              ),
-              new RaisedButton(
-                color: Colors.black,
-                onPressed: (){},
-                child: new Text('Submit',style: TextStyle(color: Colors.white),),
-              ),
-
-
+                      new RaisedButton(
+                        color: Colors.black,
+                        onPressed: (){},
+                        child: new Text('Cancel',style: TextStyle(color: Colors.white),),
+                      ),
+                      new RaisedButton(
+                        color: Colors.black,
+                        onPressed: (){},
+                        child: new Text('Submit',style: TextStyle(color: Colors.white),),
+                      ),
+                    ],
+                  ),
+                ),
+              )
 
             ],
           ),
